8ca157d7aa1f5dc04ebabd8f876a987131829602
ginabeki <ginabeki3@gmail.com>
2022-10-18 20:10:51 +0300

    Fix linters

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".menu");
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-link").forEach((element) =>
-  element.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+document.querySelectorAll('.nav-link').forEach((element) => element.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
